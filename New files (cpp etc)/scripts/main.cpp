46c1574fdaf480aa5e621fba472e9dba1bffec40
gagik <gagik.eknosyan@gmail.com>
2025-09-05 15:04:31 +0200

    new main

--- New files (cpp etc)/scripts/main.cpp
+++ New files (cpp etc)/scripts/main.cpp
@@ -1,6 +1,19 @@
+#include <Eigen/Dense>
 #include <iostream>
 
-int main(){
-    std::cout << "Test" << std::endl;
+int main() {
+    // Définir une matrice 2x2
+    Eigen::Matrix2d mat;
+    mat << 1, 2,
+           3, 4;
+
+    // Définir un vecteur 2x1
+    Eigen::Vector2d vec(5, 6);
+
+    // Multiplication matrice-vecteur
+    Eigen::Vector2d result = mat * vec;
+
+    std::cout << "Résultat de mat * vec :\n" << result << std::endl;
+
     return 0;
 }
