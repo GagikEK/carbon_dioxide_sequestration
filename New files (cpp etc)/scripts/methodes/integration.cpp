c4e10e0f0575e3477798413c7e0f5e93117209b2
gagik <gagik.eknosyan@gmail.com>
2025-09-14 23:22:07 +0200

    fixes in .gitignore + add of methodes + removal of test_eigen execuable

---
+++ New files (cpp etc)/scripts/methodes/integration.cpp
@@ -0,0 +1,17 @@
+#define h 1 //temporaire le temps de créer la class "Simulation"
+
+#include "integration.h"
+
+
+Eigen::VectorXd Integration::trapeze(Eigen::VectorXd fn, Eigen::VectorXd fn1){
+    return h * (fn + fn1)/2; 
+}
+
+Eigen::VectorXd Integration::rectangle_droite(Eigen::VectorXd fn1){
+    return h * fn1;
+}
+
+Eigen::VectorXd Integration::rectangle_gauche(Eigen::VectorXd fn){
+    return h * fn;
+}
+
